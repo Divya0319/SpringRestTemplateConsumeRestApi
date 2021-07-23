3e79ecc719c21338e397b08ef017baa201809853
Divya Gupta <divygupta0319@gmail.com>
2021-07-23 10:26:29 +0530

    Added test

---
+++ src/test/java/com/digioassignment/uploadpdf/uploadPdf/UploadPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digioassignment.uploadpdf.uploadPdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
